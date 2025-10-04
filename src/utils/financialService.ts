21aa6d9b8f1f1f894e2c1a1f24c96ed5767007f1
hamza503288 <shirifareshamza2@gmail.com>
2025-10-04 11:30:41 +0100

    Updated financialService.ts

--- src/utils/financialService.ts
+++ src/utils/financialService.ts
@@ -233,7 +233,7 @@ export const saveRistourne = async (ristourne: Ristourne): Promise<boolean> => {
         montant_ristourne: ristourne.montant_ristourne,
         date_ristourne: ristourne.date_ristourne || new Date().toISOString().split('T')[0],
            created_at:: ristourne.date_ristourne,
-        cree_par: ristourne.cree_par,
+        cree_par: ristourne.cree_par
    
       }])
       .select();
